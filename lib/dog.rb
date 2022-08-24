803cbf7062414d24ed8479d9014de53f3134110e
Elijah <bundielly@gmail.com>
2022-08-24 05:54:39 +0300

    commit tree 37efef506fff2cd384d5566c5c5b458839b26639

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,20 +29,21 @@ class Dog
     end
 
     def save
-        sql = <<-SQL
-            INSERT INTO dogs (
-                name, breed
-            )
-            VALUES (
-                ?, ?
-            )
-        SQL
+        if self.id
+            self.update
+        else
+            sql = <<-SQL
+                INSERT INTO dogs 
+                    (name, breed)
+                VALUES 
+                    (?, ?)
+            SQL
 
-        DB[:conn].execute(sql, self.name, self.breed)
+            DB[:conn].execute(sql, self.name, self.breed)
 
-        self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
-
-        self
+            self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+            self
+        end
     end
 
     def self.create(name:, breed:)
@@ -89,6 +90,16 @@ class Dog
         DB[:conn].execute(sql, id).map do |row|
             self.new_from_db(row)
         end.first
+    end
+
+    def update
+        sql = <<-SQL
+            UPDATE dogs
+            SET name = ?, breed = ?
+            WHERE id = ?
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
     end
 
 end
